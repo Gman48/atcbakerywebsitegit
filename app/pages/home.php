c3168aebdf34baee3d607a20da144907ceee63c0
Gman48 <116597720+Gman48@users.noreply.github.com>
2025-09-17 14:22:33 -0400

    Update home.php

--- app/pages/home.php
+++ app/pages/home.php
@@ -2,5 +2,5 @@
 
 <div class="page-container construction">
     <h1>We are currently baking up a new website</h1>
-    <h1>It should be frosted soon</h1>
+    <!-- <h1>It should be frosted soon</h1> -->
 </div>
