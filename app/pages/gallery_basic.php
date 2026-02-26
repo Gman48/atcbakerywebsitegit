d10ad3079871d7a6014946273702faf6aae05d16
Gman48 <116597720+Gman48@users.noreply.github.com>
2026-02-26 14:27:34 -0500

    Update gallery_basic.php

--- app/pages/gallery_basic.php
+++ app/pages/gallery_basic.php
@@ -26,7 +26,7 @@
         </div>
     </div>
 
-    <div class="event-item grid-col-span-4">
+    <div class="event-item grid-col-span-3">
         <h1 class="text">Ontario Racquet Club</h1>
         <div class="image">
             <img src="<?=ROOT?>/assets/images/ORC.jpg" alt="ORC image">
