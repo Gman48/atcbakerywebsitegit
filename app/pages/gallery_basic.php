7c16f04bb258d09edcf91162d103e7a439e78ad6
Gman48 <116597720+Gman48@users.noreply.github.com>
2026-02-26 14:22:47 -0500

    Update gallery_basic.php

--- app/pages/gallery_basic.php
+++ app/pages/gallery_basic.php
@@ -19,14 +19,14 @@
         </div>
     </div>
 
-    <div class="event-item w-3 h-2">
+    <div class="event-item w-3">
         <h1 class="text">Seasons Christmas Show International Centre</h1>
         <div class="image">
             <img src="<?=ROOT?>/assets/images/Seasons_Christmas_Show_International_Centre.jpg" alt="Intl Centre image">
         </div>
     </div>
 
-    <div class="event-item w-3">
+    <div class="event-item w-3 h-1">
         <h1 class="text">Ontario Racquet Club</h1>
         <div class="image">
             <img src="<?=ROOT?>/assets/images/ORC.jpg" alt="ORC image">
