a8fc26d60cb15d97fe55d519b86c37faec31c43a
Gman48 <116597720+Gman48@users.noreply.github.com>
2026-02-26 14:06:15 -0500

    Update gallery_basic.php

--- app/pages/gallery_basic.php
+++ app/pages/gallery_basic.php
@@ -8,35 +8,35 @@
     <div class="event-item w-2">
         <h1 class="text">St. Mildred's Lightbourne School</h1>
         <div class="image">
-            <img src="<?=ROOT?>/assets/images/StMildred'sLightbourneSchool.jpg" alt="">
+            <img src="<?=ROOT?>/assets/images/StMildred'sLightbourneSchool.jpg" alt="SML image">
         </div>
     </div>
 
     <div class="event-item">
         <h1 class="text">Toronto Premium Outlets</h1>
         <div class="image">
-            <img src="<?=ROOT?>/assets/images/TorontoPremiumOutlets.JPG" alt="">
+            <img src="<?=ROOT?>/assets/images/TorontoPremiumOutlets.JPG" alt="Toronto Premium image">
         </div>
     </div>
 
     <div class="event-item">
         <h1 class="text">Seasons Christmas Show International Centre</h1>
         <div class="image">
-            <img src="<?=ROOT?>/assets/images/SeasonsChristmasShowInternationalCentre.JPG" alt="">
+            <img src="<?=ROOT?>/assets/images/SeasonsChristmasShowInternationalCentre.JPG" alt="Intl Centre image">
         </div>
     </div>
 
     <div class="event-item w-2">
         <h1 class="text">Ontario Racquet Club</h1>
         <div class="image">
-            <img src="<?=ROOT?>/assets/images/ORC.jpg" alt="">
+            <img src="<?=ROOT?>/assets/images/ORC.jpg" alt="ORC image">
         </div>
     </div>
 
     <div class="event-item grid-col-span-2">
         <h1 class="text">Mom Market Small Arms Inspection Building</h1>
         <div class="image">
-            <img src="<?=ROOT?>/assets/images/MomMarketSmallArmsInspectionBuilding.jpg" alt="">
+            <img src="<?=ROOT?>/assets/images/MomMarketSmallArmsInspectionBuilding.jpg" alt="Mom Market image">
         </div>
     </div>
 
