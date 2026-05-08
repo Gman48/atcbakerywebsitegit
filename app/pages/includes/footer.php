4e621964e90386b678a725e9320b2de19a05d3ac
Gman48 <116597720+Gman48@users.noreply.github.com>
2026-05-08 15:35:17 -0400

    social

--- app/pages/includes/footer.php
+++ app/pages/includes/footer.php
@@ -6,15 +6,13 @@
 				<img src="<?=ROOT?>/assets/images/logo_final.png">
 			</div>
 			<p class="text-center">Around The Corner makes the most delicious, hand made treats you have ever tasted. Try some today, you won't be disappointed.</p>
+			<p class="text-center">Check us out on social</p>
 			<ul class="social-icons"> 
-			<p class="text-center">Around The Corner social Links</p>
 				<li>
 					<a class="fa-brands fa-facebook" href="https://www.facebook.com/profile.php?id=61565711605843&sk=about"></a>
-				<!-- <div class="fa fa-facebook"></div>-->
 				</li> 
 				<li>
 					<a class="fa-brands fa-instagram" href="https://www.instagram.com/_aroundthecornerbakery/"></a>
-				<div class="fa fa-twitter"></div>
 				</li>
 				<!-- <li><div class="fa fa-github"></div></li>
 				<li><div class="fa fa-youtube"></div></li> -->
